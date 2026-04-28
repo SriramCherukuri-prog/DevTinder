a95a3fe5daac49413b328c968268f669508ea5a8
SriRam Cherukuri <sriramcherukuri92@gmail.com>
2026-04-28 15:53:45 +0530

    added auth middleware of routes

---
+++ src/Middlewares/auth.js
@@ -0,0 +1,38 @@
+ const adminAuth = (req,res,next)=>{
+
+      const token = "sriram";
+
+      const isAuthorized = token === "sriram";
+
+      if(!isAuthorized){
+
+        res.status(401).send("Not Authorized.......!")
+      }
+      else{
+
+        next()
+        
+      }
+}
+
+ const userAuth = (req,res,next)=>{
+
+      const token = "sriram";
+
+      const isAuthorized = token === "sriram";
+
+      if(!isAuthorized){
+
+        res.status(401).send("Not Authorized.......!")
+      }
+      else{
+
+        next()
+        
+      }
+}
+
+module.exports = {
+    adminAuth,
+    userAuth
+}
--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@ const express = require("express");
 
 const app = express();
 
+const {adminAuth,userAuth} = require("./Middlewares/auth")
 
-app.use("/user",(req,res,next)=>{
-      
-    // res.send("Response....!")
-    next()
+
+app.use("/admin",adminAuth)
+
+// app.use("/user",userAuth) 
+
+app.get("/admin/getAllUsersData",(req,res)=>{
+    //Logic of all data fetching
+       res.send("All Data Sent")
+ 
+})
+
+app.get("/user",userAuth,(req,res,next)=>{
+    res.send("User Data sent Sucessfully...!!!!")
+})
+
+app.get("/admin/delData",(req,res)=>{
     
-},
-(req,res)=>{
-    res.send("response2")
-}
-)
+    res.send("Data has been deleted..!")
+})
+
+
 
 
 
