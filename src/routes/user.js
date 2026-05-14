af1a633c9f60d0e384eab419c8986782c6a544e8
SriRam Cherukuri <sriramcherukuri92@gmail.com>
2026-05-14 19:54:10 +0530

    Added user/connectiosn api

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const userRouter = express.Router()
 const {userAuth}  =  require("../Middlewares/auth");
 const ConnectionRequestModel = require('../models/connectionRequest');
 
+    const USER_SAFE_DATA = "firstName lastName photoUrl age gender about Skills"
+
 //Get all the pending connection request for loggedInUser
 userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
 
@@ -15,9 +17,12 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
          const connectionRequest = await ConnectionRequestModel.find({
             toUserId:loggedInUser._id,
             status:"interested"
-         }).populate("fromUserId","firstName lastName photoUrl age gender about Skils")
+         }).populate("fromUserId",USER_SAFE_DATA)
         //  .populate("fromUserId",["firstName", "lastName"])
          
+            if(connectionRequest.length === 0) {
+             return res.json({message:"No send request received..."})
+        }
 
          res.json({
             message:"Data Fetched Successfully..!",
@@ -30,6 +35,54 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
 
 
 });
+
+//Get all matched connections
+userRouter.get("/user/connections",userAuth,async(req,res)=>{
+    try{
+
+        const loggedInUser = req.userData;
+  
+
+        //sriram => venu
+        //venu accepted => venu
+        //sriram and venu have 1 conncetion
+        //hari => venu
+        //venu accepted => hari
+        //now venu have two connections with sriram and hari
+        // hari and sriram have one connection with venu
+        //here venu is fromUser or toUser but status must be acccepted  
+
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or:[
+                {toUserId:loggedInUser._id,status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA)
+
+
+        const data = connectionRequests.map(row=>{
+          
+
+            if(row.fromUserId._id.equals(loggedInUser._id)){
+               return row.toUserId
+            }
+            return row.fromUserId;
+        })
+
+        if(data.length === 0) {
+            return res.json({message:"You not have any connections Make New Friends...  "})
+        }
+
+        res.json({
+            message:"Fetch Got it",
+            data:data
+        })
+
+    }
+    catch(err){
+        res.status(400).send("ERROR Occured in user conncections: " + err.message)
+    }
+})
 
 
 module.exports = userRouter
